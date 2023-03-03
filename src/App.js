fa2337fb2fa519bf7f2e858b13169a13a8d61005
Adithya Thejas S <adithyathejass.1997@gmail.com>
2023-03-03 17:48:30 +0530

    weather-app-initials

--- src/App.js
+++ src/App.js
@@ -11,33 +11,35 @@ import {
   IoMdSnow,
   IoMdThunderstorm,
   IoMdSearch,
+  IoIosWater
 } from 'react-icons/io';
 
 import {
   BsCloudHaze2Fill,
   BsCloudDrizzleFill,
-  BsEye,
-  BsWater,
   BsThermometer,
   BsWind,
 } from 'react-icons/bs';
 
 import { TbTemperatureCelsius } from 'react-icons/tb';
-import { ImSpinner8 } from 'react-icons/im';
+
 
 // api key
 const APIkey = 'bcf2048bc3be154bded8f277f580ba2e';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const [location, setLocation] = useState('Bucharest');
+  const [location, setLocation] = useState('');
   const [inputValue, setInputValue] = useState('');
   const [animate, setAnimate] = useState(false);
   const [loading, setLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
+  const [tab, setTab] = useState('Home')
 
   const handleInput = (e) => {
     setInputValue(e.target.value);
+    console.log("E Target value : ", e.target.value)
+    setLocation(e.target.value);
   };
 
   const handleSubmit = (e) => {
@@ -45,6 +47,8 @@ const App = () => {
     if (inputValue !== '') {
       // set location
       setLocation(inputValue);
+       fetchLocation();
+      
     }
 
     // select input
@@ -67,14 +71,13 @@ const App = () => {
     e.preventDefault();
   };
 
-  // fetch the data
-  useEffect(() => {
-    // set loading to true
-    setLoading(true);
 
+  const fetchLocation = async () => {
+    setLoading(true);
+    console.log(location);
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIkey}`;
 
-    axios
+    await axios
       .get(url)
       .then((res) => {
         // set the data after 1500 ms
@@ -82,13 +85,15 @@ const App = () => {
           setData(res.data);
           // set loading to false
           setLoading(false);
+          setTab('result')
         }, 1500);
+        
       })
       .catch((err) => {
         setLoading(false);
         setErrorMsg(err);
       });
-  }, [location]);
+  }
 
   // error message
   useEffect(() => {
@@ -100,53 +105,48 @@ const App = () => {
   }, [errorMsg]);
 
   // if data is false show the loader
-  if (!data) {
-    return (
-      <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center'>
-        <div>
-          <ImSpinner8 className='text-5xl animate-spin text-white' />
-        </div>
-      </div>
-    );
-  }
+  // if (!data) {
+  //   return (
+  //     <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center'>
+  //       <div>
+  //         <ImSpinner8 className='text-5xl animate-spin text-white' />
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   // set the icon according to the weather
   let icon;
 
-  switch (data.weather[0].main) {
-    case 'Clouds':
-      icon = <IoMdCloudy />;
-      break;
-    case 'Haze':
-      icon = <BsCloudHaze2Fill />;
-      break;
-    case 'Rain':
-      icon = <IoMdRainy className='text-[#31cafb]' />;
-      break;
-    case 'Clear':
-      icon = <IoMdSunny className='text-[#ffde33]' />;
-      break;
-    case 'Drizzle':
-      icon = <BsCloudDrizzleFill className='text-[#31cafb]' />;
-      break;
-    case 'Snow':
-      icon = <IoMdSnow className='text-[#31cafb]' />;
-      break;
-    case 'Thunderstorm':
-      icon = <IoMdThunderstorm />;
-      break;
-  }
+  // switch (data.weather[0].main) {
+  //   case 'Clouds':
+  //     icon = <IoMdCloudy />;
+  //     break;
+  //   case 'Haze':
+  //     icon = <BsCloudHaze2Fill />;
+  //     break;
+  //   case 'Rain':
+  //     icon = <IoMdRainy className='text-[#31cafb]' />;
+  //     break;
+  //   case 'Clear':
+  //     icon = <IoMdSunny className='text-[#ffde33]' />;
+  //     break;
+  //   case 'Drizzle':
+  //     icon = <BsCloudDrizzleFill className='text-[#31cafb]' />;
+  //     break;
+  //   case 'Snow':
+  //     icon = <IoMdSnow className='text-[#31cafb]' />;
+  //     break;
+  //   case 'Thunderstorm':
+  //     icon = <IoMdThunderstorm />;
+  //     break;
+  // }
 
-  // date object
-  const date = new Date();
+
 
   return (
     <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0'>
-      {errorMsg && (
-        <div className='w-full max-w-[90vw] lg:max-w-[450px] bg-[#ff208c] text-white absolute top-2 lg:top-10 p-4 capitalize rounded-md'>{`${errorMsg.response.data.message}`}</div>
-      )}
-      {/* form */}
-      <form
+      {tab === 'Home' && <form
         className={`${
           animate ? 'animate-shake' : 'animate-none'
         } h-16 bg-black/30 w-full max-w-[450px]
@@ -157,7 +157,7 @@ const App = () => {
             onChange={(e) => handleInput(e)}
             className='flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full'
             type='text'
-            placeholder='Search by city or country'
+            placeholder='Enter city name'
           />
           <button
             onClick={(e) => handleSubmit(e)}
@@ -166,14 +166,9 @@ const App = () => {
             <IoMdSearch className='text-2xl text-white' />
           </button>
         </div>
-      </form>
-      {/* card */}
+      </form>}
+       {tab === 'result' && 
       <div className='w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px] py-12 px-6'>
-        {loading ? (
-          <div className='w-full h-full flex justify-center items-center'>
-            <ImSpinner8 className='text-white text-5xl animate-spin' />
-          </div>
-        ) : (
           <div>
             {/* card top */}
             <div className='flex items-center gap-x-5'>
@@ -184,11 +179,7 @@ const App = () => {
                 <div className='text-2xl font-semibold'>
                   {data.name}, {data.sys.country}
                 </div>
-                {/* date */}
-                <div>
-                  {date.getUTCDate()}/{date.getUTCMonth() + 1}/
-                  {date.getUTCFullYear()}
-                </div>
+                
               </div>
             </div>
             {/* card body */}
@@ -208,17 +199,16 @@ const App = () => {
                 {data.weather[0].description}
               </div>
             </div>
-            {/* card bottom */}
+          {/* card bottom */}
             <div className='max-w-[378px] mx-auto flex flex-col gap-y-6'>
               <div className='flex justify-between'>
                 <div className='flex items-center gap-x-2'>
                   {/* icon */}
                   <div className='text-[20px]'>
-                    <BsEye />
+                    <IoIosWater />
                   </div>
                   <div>
-                    Visibility{' '}
-                    <span className='ml-2'>{data.visibility / 1000} km</span>
+                    Wind <span className='ml-2'>{data.wind.speed} m/s</span>
                   </div>
                 </div>
                 <div className='flex items-center gap-x-2'>
@@ -236,30 +226,11 @@ const App = () => {
                 </div>
               </div>
               <div className='flex justify-between'>
-                <div className='flex items-center gap-x-2'>
-                  {/* icon */}
-                  <div className='text-[20px]'>
-                    <BsWater />
-                  </div>
-                  <div>
-                    Humidity
-                    <span className='ml-2'>{data.main.humidity} %</span>
-                  </div>
-                </div>
-                <div className='flex items-center gap-x-2'>
-                  {/* icon */}
-                  <div className='text-[20px]'>
-                    <BsWind />
-                  </div>
-                  <div>
-                    Wind <span className='ml-2'>{data.wind.speed} m/s</span>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
-        )}
-      </div>
+        
+      </div> }
     </div>
   );
 };
